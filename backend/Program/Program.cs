dc141b32a3645b8e9f1a591f6f89fdba0286764f
ifgamek <andkategik@gmail.com>
2026-09-21 18:59:09 +0300

    Add user name input and greeting

--- backend/Program/Program.cs
+++ backend/Program/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("enter your name:");
+        string? name = Console.ReadLine();
         Console.WriteLine("Hello, World!");
     }
 }
